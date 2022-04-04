b23af47d9c9ff52eeb389e28b16be09e9b673e64
Cottonwood <blog_geon@naver.com>
2022-04-05 04:07:15 +0900

    add tailwindcss-styled-components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 
 interface LayoutProps {
   title?: string;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
-
+import tw from "tailwind-styled-components";
+const HelloWorld = tw.div`
+bg-gray-700 w-full h-[100vh] flex justify-center items-center font-bold text-white text-6xl
+`;
 const Home: NextPage = () => {
   return (
     <Layout title="타이틀">
-      <div className="bg-gray-700 w-full h-[100vh] flex justify-center items-center font-bold text-white text-6xl">
-        Hello world!
-      </div>
+      <HelloWorld>
+        <span>Hello world!</span>
+      </HelloWorld>
     </Layout>
   );
 };

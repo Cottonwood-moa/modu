02cdf87a09ce04c40cc4afadeaee449900ddc3c2
Cottonwood <blog_geon@naver.com>
2022-05-05 16:30:11 +0900

    no message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,47 @@ import type { AppProps } from "next/app";
 import { RecoilRoot, useRecoilValue } from "recoil";
 import { SWRConfig } from "swr";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 import "@fontsource/gugi";
 import "@fontsource/noto-sans-kr";
 const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider
-      // Provider options are not required but can be useful in situations where
-      // you have a short session maxAge time. Shown here with default values.
-      session={pageProps?.session}
-    >
-      <SWRConfig value={{ fetcher }}>
-        <RecoilRoot>
-          <div>
-            <Component {...pageProps} />
-          </div>
-        </RecoilRoot>
-      </SWRConfig>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>modu</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="modu" />
+        <meta property="og:title" content="modu" />
+        <meta property="og:description" content="블라블라" />
+        <meta property="og:image" content="/images/modu.png" />
+        <meta property="og:url" content="https://modu.vercel.app" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:site" content="modu" />
+        <meta property="twitter:title" content="modu" />
+        <meta property="twitter:description" content="블라블라" />
+        <meta property="twitter:image" content="/images/modu.png" />
+        <meta property="twitter:url" content="https://modu.vercel.app" />
+        <meta
+          name="google-site-verification"
+          content="jP_LRgp2ourifn-dveaqSx3v-cBhd7cwuHbUlM6bsA4"
+        />
+      </Head>
+      <SessionProvider
+        // Provider options are not required but can be useful in situations where
+        // you have a short session maxAge time. Shown here with default values.
+        session={pageProps?.session}
+      >
+        <SWRConfig value={{ fetcher }}>
+          <RecoilRoot>
+            <div>
+              <Component {...pageProps} />
+            </div>
+          </RecoilRoot>
+        </SWRConfig>
+      </SessionProvider>
+    </>
   );
 }
 

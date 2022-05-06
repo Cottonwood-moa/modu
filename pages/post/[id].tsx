00a7fe8dc3d0391790e0b1ecc60dbbfe2c427fbe
Cottonwood <blog_geon@naver.com>
2022-05-07 03:51:16 +0900

    add lazyhydration

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -27,6 +27,7 @@ import { orderAtom, pageAtom } from "@atom/atom";
 import { useRecoilState } from "recoil";
 import ImageDelivery from "@libs/client/imageDelivery";
 import numberWithCommas from "@libs/client/numberWithComma";
+import LazyHydrate from "react-lazy-hydration";
 const ReactMarkdown = dynamic(() => import("react-markdown"), { ssr: false });
 const PostComment = dynamic(() => import("@components/PostComment"), {
   ssr: false,
@@ -228,219 +229,223 @@ const PostDetail: NextPage<staticProps> = ({
           className="right-0 left-0 m-auto mt-32 w-[60%] min-w-[800px] bg-white p-12 text-gray-800 dark:bg-slate-900 dark:text-white "
         >
           {/* post header */}
-          <div className="space-y-12">
-            <div className="flex  w-full items-center justify-between">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12 cursor-pointer"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-                onClick={() => router.back()}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
-                />
-              </svg>
-              <div className="flex items-center space-x-2">
-                <div className="flex items-center space-x-1 text-gray-800 dark:text-white">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-12 w-12"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentcolor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          <LazyHydrate whenVisible>
+            <div className="space-y-12">
+              <div className="flex  w-full items-center justify-between">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-12 w-12 cursor-pointer"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  onClick={() => router.back()}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
+                  />
+                </svg>
+                <div className="flex items-center space-x-2">
+                  <div className="flex items-center space-x-1 text-gray-800 dark:text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-12 w-12"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentcolor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>
+                    <span className="text-xl font-bold">
+                      {numberWithCommas(views)}
+                    </span>
+                  </div>
+
+                  {data?.liked ? (
+                    <div className="flex items-center">
+                      <motion.div
+                        layoutId="liked"
+                        onClick={likeClickHandle}
+                        animate={{ rotate: 360 }}
+                        whileHover={{ scale: 1.2 }}
+                        className={
+                          "flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-red-400"
+                        }
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-8 w-8"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                          />
+                        </svg>
+                      </motion.div>
+                      <div className="mx-2 text-3xl font-bold">
+                        {count ? count?.count?.favs : 0}
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex items-center">
+                      <motion.div
+                        layoutId="liked"
+                        onClick={likeClickHandle}
+                        animate={{ rotate: 0 }}
+                        whileHover={{ scale: 1.2 }}
+                        className={
+                          "flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gray-400"
+                        }
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-8 w-8"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                          />
+                        </svg>
+                      </motion.div>
+                      <div className="mx-2 text-3xl font-bold">
+                        {count ? count?.count?.favs : 0}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+              {/* title */}
+              <div className="w-full text-3xl font-bold xl:text-4xl 2xl:text-5xl">
+                {title}
+              </div>
+              {userId === user?.id ? (
+                <div className="flex items-center space-x-2 text-xl font-bold text-gray-400">
+                  <div className="cursor-pointer " onClick={goToEdit}>
+                    수정
+                  </div>
+                  <span>/</span>
+                  <div onClick={postDeleteHandle} className="cursor-pointer ">
+                    삭제
+                  </div>
+                </div>
+              ) : (
+                <></>
+              )}
+              {/* user & date */}
+              <div className="flex w-full items-center justify-between">
+                <div
+                  className="flex cursor-pointer items-center space-x-2"
+                  onClick={() => router.push(`/myPage/${userId}`)}
+                >
+                  {avatar.includes("https") ? (
+                    <img
+                      src={avatar}
+                      className="h-12 w-12 rounded-full bg-slate-600"
+                      alt=""
                     />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  ) : (
+                    <img
+                      src={ImageDelivery(avatar, "avatar")}
+                      className="h-12 w-12 rounded-full bg-slate-600"
+                      alt=""
                     />
-                  </svg>
-                  <span className="text-xl font-bold">
-                    {numberWithCommas(views)}
+                  )}
+                  <span className="text-lg font-semibold xl:text-2xl ">
+                    {name}
                   </span>
                 </div>
-
-                {data?.liked ? (
-                  <div className="flex items-center">
-                    <motion.div
-                      layoutId="liked"
-                      onClick={likeClickHandle}
-                      animate={{ rotate: 360 }}
-                      whileHover={{ scale: 1.2 }}
-                      className={
-                        "flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-red-400"
-                      }
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-8 w-8"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                        />
-                      </svg>
-                    </motion.div>
-                    <div className="mx-2 text-3xl font-bold">
-                      {count ? count?.count?.favs : 0}
-                    </div>
-                  </div>
-                ) : (
-                  <div className="flex items-center">
-                    <motion.div
-                      layoutId="liked"
-                      onClick={likeClickHandle}
-                      animate={{ rotate: 0 }}
-                      whileHover={{ scale: 1.2 }}
-                      className={
-                        "flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gray-400"
-                      }
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-8 w-8"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                        />
-                      </svg>
-                    </motion.div>
-                    <div className="mx-2 text-3xl font-bold">
-                      {count ? count?.count?.favs : 0}
-                    </div>
-                  </div>
-                )}
-              </div>
-            </div>
-            {/* title */}
-            <div className="w-full text-3xl font-bold xl:text-4xl 2xl:text-5xl">
-              {title}
-            </div>
-            {userId === user?.id ? (
-              <div className="flex items-center space-x-2 text-xl font-bold text-gray-400">
-                <div className="cursor-pointer " onClick={goToEdit}>
-                  수정
-                </div>
-                <span>/</span>
-                <div onClick={postDeleteHandle} className="cursor-pointer ">
-                  삭제
+                <div className="text-lg font-semibold text-slate-500 dark:text-white xl:text-2xl">
+                  {ParsingCreatedAt(createdAt)}
                 </div>
               </div>
-            ) : (
-              <></>
-            )}
-            {/* user & date */}
-            <div className="flex w-full items-center justify-between">
-              <div
-                className="flex cursor-pointer items-center space-x-2"
-                onClick={() => router.push(`/myPage/${userId}`)}
-              >
-                {avatar.includes("https") ? (
-                  <img
-                    src={avatar}
-                    className="h-12 w-12 rounded-full bg-slate-600"
-                    alt=""
-                  />
-                ) : (
-                  <img
-                    src={ImageDelivery(avatar, "avatar")}
-                    className="h-12 w-12 rounded-full bg-slate-600"
-                    alt=""
-                  />
-                )}
-                <span className="text-lg font-semibold xl:text-2xl ">
-                  {name}
-                </span>
-              </div>
-              <div className="text-lg font-semibold text-slate-500 dark:text-white xl:text-2xl">
-                {ParsingCreatedAt(createdAt)}
+              {/* tag */}
+              <div className="space-x-2 border-b-2 pb-8">
+                {tags?.map((tag, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="m-1 inline-block w-auto rounded-xl bg-slate-500 py-1 px-2 font-bold text-white"
+                    >
+                      # {tag?.tag?.name}
+                    </div>
+                  );
+                })}
               </div>
             </div>
-            {/* tag */}
-            <div className="space-x-2 border-b-2 pb-8">
-              {tags?.map((tag, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="m-1 inline-block w-auto rounded-xl bg-slate-500 py-1 px-2 font-bold text-white"
-                  >
-                    # {tag?.tag?.name}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          </LazyHydrate>
           {/* post content */}
           {content && (
-            <div className="post-content py-12">
-              <ReactMarkdown
-                className="react-markdown"
-                rehypePlugins={[rehypeRaw]}
-                remarkPlugins={[remarkGfm]}
-                children={content?.replace(/\n/g, "\n\n")}
-                components={{
-                  code({ node, inline, className, children, ...props }) {
-                    const match = /language-(\w+)/.exec(className || "");
-                    return !inline && match ? (
-                      <SyntaxHighlighter
-                        children={String(children).replace(/\n\n/g, "\n")}
-                        style={a11yDark}
-                        language={match[1]}
-                        PreTag="main"
-                        {...props}
-                      />
-                    ) : (
-                      <SyntaxHighlighter
-                        children={String(children).replace(/\n\n/g, "\n")}
-                        style={a11yDark}
-                        language="textile"
-                        PreTag="main"
-                        {...props}
-                      />
-                    );
-                  },
-                  main({ node, children, ...props }) {
-                    return (
-                      // @ts-ignore
-                      <div className="code" {...props}>
-                        {children}
-                      </div>
-                    );
-                  },
-                  // 인용문
-                  blockquote({ node, children, ...props }) {
-                    return (
-                      // @ts-ignore
-                      <div className="block-quote" {...props}>
-                        {children}
-                      </div>
-                    );
-                  },
-                }}
-              />
-            </div>
+            <LazyHydrate whenVisible>
+              <div className="post-content py-12">
+                <ReactMarkdown
+                  className="react-markdown"
+                  rehypePlugins={[rehypeRaw]}
+                  remarkPlugins={[remarkGfm]}
+                  children={content?.replace(/\n/g, "\n\n")}
+                  components={{
+                    code({ node, inline, className, children, ...props }) {
+                      const match = /language-(\w+)/.exec(className || "");
+                      return !inline && match ? (
+                        <SyntaxHighlighter
+                          children={String(children).replace(/\n\n/g, "\n")}
+                          style={a11yDark}
+                          language={match[1]}
+                          PreTag="main"
+                          {...props}
+                        />
+                      ) : (
+                        <SyntaxHighlighter
+                          children={String(children).replace(/\n\n/g, "\n")}
+                          style={a11yDark}
+                          language="textile"
+                          PreTag="main"
+                          {...props}
+                        />
+                      );
+                    },
+                    main({ node, children, ...props }) {
+                      return (
+                        // @ts-ignore
+                        <div className="code" {...props}>
+                          {children}
+                        </div>
+                      );
+                    },
+                    // 인용문
+                    blockquote({ node, children, ...props }) {
+                      return (
+                        // @ts-ignore
+                        <div className="block-quote" {...props}>
+                          {children}
+                        </div>
+                      );
+                    },
+                  }}
+                />
+              </div>
+            </LazyHydrate>
           )}
 
           <PostComment

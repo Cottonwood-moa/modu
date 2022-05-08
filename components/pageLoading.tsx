cc3f47d969a2a85c7b3ce95ae3b550298dba1ebd
Cottonwood <blog_geon@naver.com>
2022-05-09 03:48:31 +0900

    페이지 이동 간 로딩 추가

--- components/pageLoading.tsx
+++ components/pageLoading.tsx
@@ -1,7 +1,17 @@
+import { darkModeAtom } from "@atom/atom";
+import { cls } from "@libs/client/utils";
+import { useRecoilValue } from "recoil";
+
 /* eslint-disable @next/next/no-img-element */
 export default function PageLoading() {
+  const isDark = useRecoilValue(darkModeAtom);
   return (
-    <div className="bg-dark fixed z-20 flex h-[100vh] w-[1600px] flex-col items-center justify-center font-bold text-gray-800 ">
+    <div
+      className={cls(
+        `fixed z-20 flex h-[100vh] w-[100%] flex-col items-center justify-center font-bold text-gray-800`,
+        isDark ? "bg-slate-900" : "bg-white"
+      )}
+    >
       <img src="/images/loading.gif" alt="" />
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,25 @@ import { SWRConfig } from "swr";
 import { SessionProvider } from "next-auth/react";
 import "@fontsource/gugi";
 import "@fontsource/noto-sans-kr";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import PageLoading from "@components/pageLoading";
 const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [pageLoading, setLoading] = useState(false);
+  useEffect(() => {
+    const handleStart = () => {
+      setLoading(true);
+    };
+    const handleComplete = () => {
+      setLoading(false);
+    };
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
   return (
     <SessionProvider
       // Provider options are not required but can be useful in situations where
@@ -16,9 +32,13 @@ function MyApp({ Component, pageProps }: AppProps) {
     >
       <SWRConfig value={{ fetcher }}>
         <RecoilRoot>
-          <div>
-            <Component {...pageProps} />
-          </div>
+          {pageLoading ? (
+            <PageLoading />
+          ) : (
+            <div>
+              <Component {...pageProps} />
+            </div>
+          )}
         </RecoilRoot>
       </SWRConfig>
     </SessionProvider>

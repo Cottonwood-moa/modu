28977cd8b181927d480663e792e0619f2e5f1997
Cottonwood <blog_geon@naver.com>
2022-05-11 01:40:11 +0900

    no message

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -10,11 +10,11 @@ const generateSitemap = (data, origin) => {
     </url>`;
   });
 
-  return ` <?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-   xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+ xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${xml}
     </urlset>`;
 };
@@ -31,6 +31,7 @@ export async function getServerSideProps({ res }) {
       lastMod: getDate,
     };
   });
+  /* do some async fetching here */
   const data = {
     pages: [
       {
